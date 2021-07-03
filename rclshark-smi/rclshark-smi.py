28edd62e3295dbcfc9040ed76d268974f4dc0498
Ar-Ray-code <ray255ar@gmail.com>
2021-07-04 03:55:57 +0900

    add rclshark-smi

--- rclshark-smi/rclshark-smi.py
+++ rclshark-smi/rclshark-smi.py
@@ -3,7 +3,10 @@ import subprocess
 import os
 import re
 import signal
-import asyncio
+import time
+import threading
+import sys
+import select
 
 import rclpy
 from rclpy.node import Node
@@ -17,9 +20,11 @@ version_revision = 0
 version_build = str(version_mejor) + "." + str(version_minor) + "." + str(version_revision)
 
 global_data = list()
-srv_list = list()
+srv_class_list = list()
+ip_list = list()
 
-signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+# ==========================================================
 
 class using_srv(Node):
     def __init__(self, _ip:str):
@@ -28,11 +33,13 @@ class using_srv(Node):
         self.sub_pc_0 = 'ip_'+self.ip+'_endcb'
 
     def __dell__(self):
+        self.destroy_node()
         self.get_status.destroy()
+        del self.req
 
     def reset_client(self) -> bool:
         self.get_status = self.create_client(PcStatusSrv, '/'+self.sub_pc_0)
-        while not self.get_status.wait_for_service(timeout_sec=1.0):
+        if not self.get_status.wait_for_service(timeout_sec=0.2):
             self.get_logger().info('service not available, waiting again...')
             return 1
         self.req = PcStatusSrv.Request()
@@ -47,13 +54,13 @@ class srv_main:
         self.ip = _ip
         self.ros_class = using_srv(self.ip)
     def __dell__(self):
-        del self.ros_class
+        self.ros_class.destroy_node()
     
     def using_srv_fnc(self):
         if(self.ros_class.reset_client()):
             return 1
         self.req = self.ros_class.get_by_service()
-        while rclpy.ok():
+        if rclpy.ok():
             rclpy.spin_once(self.ros_class)
             if self.ros_class.future.done():
                 try:
@@ -62,11 +69,11 @@ class srv_main:
                     rclpy.spin_once(self.ros_class)
                 except Exception as e:
                     print(e)
-            break
+            # break
         return 0
 
 def show_data():
-    global global_data
+    global_data
 
     print("+============================================================================+")
     print("| RCLSHARK-SMI " + version_build + "\t" + "ROS-DISTRO " + os.environ['ROS_DISTRO'] + "\t\t\t\t\t     |")
@@ -88,17 +95,16 @@ def show_data():
         print("|\t\t\t\t\t\t\t\t\t     |")
     print("+============================================================================+")
 
-def get_from_srv(srv_list:list, ip_list:list):
+def get_from_srv(srv_list:list, _ip_list:list):
     # get data from srv_list
     for i in range(len(srv_list)):
         try:
             out = srv_list[i].using_srv_fnc()
-            print(out)
             if(out):
-                ip_list.remove(srv_list[i].ip)
+                _ip_list.remove(srv_list[i].ip)
         except:
             pass
-    return srv_list, ip_list
+    return srv_list, _ip_list
 
 def get_ip_list() -> list:
     try:
@@ -106,36 +112,52 @@ def get_ip_list() -> list:
     except:
         return []
 
-async def loop():
-    global srv_list
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
+def loop():
+    global ip_list
+    global srv_class_list
+    
     ip_list = get_ip_list()
         
     # append ip_list to srv_list
-    if(len(ip_list) > len(srv_list)):
-        srv_list.clear()
+    if(len(ip_list) > len(srv_class_list)):
+        srv_class_list.clear()
         for i in ip_list:
-            srv_list.append(srv_main(i))
-    elif (len(ip_list) < len(srv_list)):
-        srv_list.clear()
-        ip_list.clear()
-        return
+            srv_class_list.append(srv_main(i))
 
-    print(ip_list)
-    print(srv_list)
-    srv_list, ip_list = get_from_srv(srv_list, ip_list)
-    # show_data()
+    srv_class_list, ip_list = get_from_srv(srv_class_list, ip_list)
+
+    show_data()
     global_data.clear()
 
-async def asyncio_loop():
-    task = asyncio.create_task(loop())
-    await asyncio.wait_for(asyncio.shield(task), timeout=2)
+def input_timeout(timeout=10):
+    (ready, _, _) = select.select([sys.stdin], [], [], timeout)
+    if ready:
+        return sys.stdin.readline().rstrip('\n')
+    else:
+        return ''
 
 def ros_main(args = None):
+    global ip_list
+    global srv_list
     rclpy.init(args=args)
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     while rclpy.ok():
-        asyncio.run(asyncio_loop())
+        
+        t = threading.Thread(target=loop,args=())
+        t.setDaemon(True)
+        t.start()
+        t.join(timeout=5.0)
+
+        if t.is_alive():
+            ip_list.clear()
+            for srv in srv_class_list:
+                del srv
+            exit(1)
+        if input_timeout(0.1) == 'q':
+            exit(1)
+            
+        del t
     rclpy.shutdown()
 
 if __name__=='__main__':

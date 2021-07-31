c5b2974717e7769f3dfd46f53a26b6fe9def326d
Ar-Ray-code <ray255ar@gmail.com>
2021-07-31 10:54:06 +0900

    rm build

--- rclsharkc/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/ros2_ws/src/rclshark/rclsharkc
-# Build directory: /home/ubuntu/ros2_ws/src/rclshark/rclsharkc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

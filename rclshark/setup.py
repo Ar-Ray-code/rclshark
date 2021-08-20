080bfb7ed62e1b36960406634fe2aa10ac9ff77b
Ar-Ray-code <ray255ar@gmail.com>
2021-08-21 08:42:45 +0900

    update rclshark-smi

--- rclshark/setup.py
+++ rclshark/setup.py
@@ -5,9 +5,11 @@ package_name = 'rclshark'
 setup(
     name=package_name,
     version='1.0.1',
-    packages=[],
-    py_modules= [
-        'scripts.rclshark',
+    packages=['rclshark'],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -27,7 +29,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'rclshark = scripts.rclshark:ros_main',
+            'rclshark = rclshark.rclshark:ros_main',
         ],
     }
 )
